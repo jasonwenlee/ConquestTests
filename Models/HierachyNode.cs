1ac7e38c45373b4563a8ab76077cac51243ef073
Jason <jasonleewl1994@gmail.com>
2020-06-10 15:15:14 +0930

    Working on create request

---
+++ Models/HierachyNode.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConquestTests.Models
+{
+    public class HierachyNode
+    {
+        [JsonProperty("IncludeAncestors")]
+        public bool IncludeAncestors { get; set; }
+
+        [JsonProperty("IncludeChildren")]
+        public bool IncludeChildren { get; set; }
+
+        [JsonProperty("IncludeDescendents")]
+        public long IncludeDescendents { get; set; }
+
+        [JsonProperty("IncludeSiblings")]
+        public bool IncludeSiblings { get; set; }
+
+        [JsonProperty("IncludeSubItems")]
+        public bool IncludeSubItems { get; set; }
+
+        [JsonProperty("ObjectKey")]
+        public ObjectKey ObjectKey { get; set; }
+    }
+
+    public class ObjectKey
+    {
+        [JsonProperty("int32Value")]
+        public long Int32Value { get; set; }
+
+        [JsonProperty("objectType")]
+        public string ObjectType { get; set; }
+
+        [JsonProperty("stringValue")]
+        public string StringValue { get; set; }
+
+        [JsonProperty("timestampValue")]
+        public DateTimeOffset TimestampValue { get; set; }
+    }
+}
--- RequestTest.cs
+++ RequestTest.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using ConquestTests.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace ConquestTests
 {
@@ -11,42 +13,86 @@ namespace ConquestTests
     public class RequestTest
     {
         [TestMethod]
-        public void EstablishConnection()
+        public async System.Threading.Tasks.Task EstablishConnectionAsync()
         {
-
             var ACCESS_TOKEN = "RO7B15hxNvRZGasvXOR9G/10+Jk=";
             HttpClient client = new HttpClient
             {
                 BaseAddress = new Uri("https://developer-demo.australiaeast.cloudapp.azure.com/")
             };
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
-
         }
 
         [TestMethod]
         public async System.Threading.Tasks.Task GetRequestAsync()
         {
-
-            var value = 91;
+            var requestID = 91;
             var ACCESS_TOKEN = "RO7B15hxNvRZGasvXOR9G/10+Jk=";
-            var APIPATH = $"/api/requests/{value}";
+            var APIPATH = $"/api/requests/{requestID}";
             string responseDetail = "";
             HttpClient client = new HttpClient
             {
                 BaseAddress = new Uri("https://developer-demo.australiaeast.cloudapp.azure.com/")
             };
+            // Address SSL and TLS security issue.
+            #region
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 |
                                                     SecurityProtocolType.Tls | 
                                                     SecurityProtocolType.Tls11 | 
                                                     SecurityProtocolType.Tls12;
+            #endregion
 
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+            // GET request
             HttpResponseMessage response = await client.GetAsync(APIPATH);
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
             {
-                responseDetail = response.Content.ReadAsStringAsync().Result;
+                throw new System.ArgumentException(response.StatusCode.ToString(), "original");
             }
-            Debug.Write(responseDetail);
+            responseDetail = response.Content.ReadAsStringAsync().Result;
+            var netObject = JsonConvert.DeserializeObject(responseDetail);
+            // Response body
+            Debug.WriteLine(netObject);
+            Debug.WriteLine("");
+            // Response code
+            Debug.WriteLine(response);
+        }
+
+        [TestMethod]
+        public async System.Threading.Tasks.Task CreateRequestAsync()
+        {
+            var ACCESS_TOKEN = "RO7B15hxNvRZGasvXOR9G/10+Jk=";
+            var APIHIERACHYNODES = "/api/list_hierarchy_nodes";
+            string responseDetail = "";
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://developer-demo.australiaeast.cloudapp.azure.com/")
+            };
+
+            // Address SSL and TLS security issue.
+            #region
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 |
+                                                    SecurityProtocolType.Tls |
+                                                    SecurityProtocolType.Tls11 |
+                                                    SecurityProtocolType.Tls12;
+            #endregion
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+
+            ObjectKey key = new ObjectKey() { Int32Value = 0, ObjectType = "ObjectType_OrganisationUnit", StringValue = "string", TimestampValue = DateTime.Now };
+            HierachyNode node = new HierachyNode() { IncludeAncestors = true, IncludeChildren = true, IncludeDescendents = 0, IncludeSiblings = true, IncludeSubItems = true };
+            node.ObjectKey = key;
+            var json = JsonConvert.SerializeObject(node);
+
+            Debug.WriteLine(json);
+            HttpResponseMessage response = await client.PostAsJsonAsync(APIHIERACHYNODES,json);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new System.ArgumentException(response.StatusCode.ToString(), "original");
+            }
+            //responseDetail = response.Content.ReadAsStringAsync().Result;
+
+            //Debug.WriteLine(responseDetail);
         }
     }
 }
