70bcb3aa4da8e7df4a0b85c55413aefd8a1dd174
Jason <jasonleewl1994@gmail.com>
2020-06-10 13:19:32 +0930

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConquestTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConquestTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16bb47fd-7f63-4442-bae7-f1fc5f31bc99")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RequestTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConquestTests
+{
+    [TestClass]
+    public class RequestTest
+    {
+        [TestMethod]
+        public void EstablishConnection()
+        {
+
+            var ACCESS_TOKEN = "RO7B15hxNvRZGasvXOR9G/10+Jk=";
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://developer-demo.australiaeast.cloudapp.azure.com/")
+            };
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+
+        }
+
+        [TestMethod]
+        public async System.Threading.Tasks.Task GetRequestAsync()
+        {
+
+            var value = 91;
+            var ACCESS_TOKEN = "RO7B15hxNvRZGasvXOR9G/10+Jk=";
+            var APIPATH = $"/api/requests/{value}";
+            string responseDetail = "";
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://developer-demo.australiaeast.cloudapp.azure.com/")
+            };
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 |
+                                                    SecurityProtocolType.Tls | 
+                                                    SecurityProtocolType.Tls11 | 
+                                                    SecurityProtocolType.Tls12;
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+            HttpResponseMessage response = await client.GetAsync(APIPATH);
+            if (response.IsSuccessStatusCode)
+            {
+                responseDetail = response.Content.ReadAsStringAsync().Result;
+            }
+            Debug.Write(responseDetail);
+        }
+    }
+}
